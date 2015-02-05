730a381cde899e5296b000d7b280259a00d78787
Paul Hiatt <hiattp@gmail.com>
2015-02-05 18:33:48 +0700

    moved tasks directory

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 ENV['DRAGON_ENV'] ||= 'production'
 require File.expand_path("../config/environment", __FILE__)
-rake_tasks = File.join(File.dirname(__FILE__), 'lib', '**', '*.rake')
+rake_tasks = File.join(File.dirname(__FILE__), 'tasks', '**', '*.rake')
 Dir[rake_tasks].each { |file| load file }
 
 task default: ['sync:clear', 'sync:create']
--- spec/support/shared_contexts/rake.rb
+++ spec/support/shared_contexts/rake.rb
@@ -4,7 +4,7 @@ shared_context "rake" do
   let(:rake)      { Rake::Application.new }
   let(:project_directory) { File.join(File.dirname(__FILE__), '../../../') }
   let(:task_name) { self.class.top_level_description }
-  let(:task_path) { "lib/tasks/#{task_name.split(":").first}" }
+  let(:task_path) { "tasks/#{task_name.split(":").first}" }
   subject         { rake[task_name] }
 
   def loaded_files_excluding_current_rake_file
