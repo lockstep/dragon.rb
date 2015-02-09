524d8947c0d8d497a1eb92e839d6a6c10a35f521
Paul Hiatt <hiattp@gmail.com>
2015-02-09 19:05:46 +0700

    added vim nav commands

--- lib/navigation/vim_navigation.rb
+++ lib/navigation/vim_navigation.rb
@@ -19,5 +19,23 @@ module Navigation
       end
     end
 
+    class UpWindow < KeystrokeBuilder
+      def initialize
+        @action = 'Control-k'
+      end
+    end
+
+    class DownWindow < KeystrokeBuilder
+      def initialize
+        @action = 'Control-j'
+      end
+    end
+
+    class FileTop < TextMacroBuilder
+      def initialize
+        @action = 'gg'
+      end
+    end
+
   end
 end
--- spec/lib/navigation/vim_navigation_spec.rb
+++ spec/lib/navigation/vim_navigation_spec.rb
@@ -23,4 +23,28 @@ describe 'Vim Navigation' do
       expect(@builder.action).to eq 'Control-h'
     end
   end
+
+  describe Navigation::Vim::UpWindow do
+    before { @builder = Navigation::Vim::UpWindow.new }
+    it_behaves_like 'a keystroke'
+    it 'hits control k to navigate a window upwards' do
+      expect(@builder.action).to eq 'Control-k'
+    end
+  end
+
+  describe Navigation::Vim::DownWindow do
+    before { @builder = Navigation::Vim::DownWindow.new }
+    it_behaves_like 'a keystroke'
+    it 'hits control j to navigate a window downwards' do
+      expect(@builder.action).to eq 'Control-j'
+    end
+  end
+
+  describe Navigation::Vim::FileTop do
+    before { @builder = Navigation::Vim::FileTop.new }
+    it_behaves_like 'a text macro'
+    it 'hits gg to navigate to the files top' do
+      expect(@builder.action).to eq 'gg'
+    end
+  end
 end
